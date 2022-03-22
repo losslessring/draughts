ea589ab5b4006cffbdcb8c2980d1a1245ec28681
Andrey <losslessring@yandex.ru>
2022-03-22 21:10:37 +0300

    initial commit

---
+++ src/components/Cell/Cell.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const style = {
+    height: 300,
+    width: 300,
+    border: 'solid',
+    borderWidth: '2px',
+    borderColor: 'blue',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+const Cell = (props: any) => {
+    return (
+        <div style={style} onClick={() => console.log('clicked') }></div>
+    )
+
+}
+
+        
+
+export default Cell
